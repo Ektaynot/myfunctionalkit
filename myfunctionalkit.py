678007da483ad9eb3051b3221c9fecfdb19c50ff
Ektaynot <40837206+Ektaynot@users.noreply.github.com>
2020-03-12 18:34:40 +0300

    Update myfunctionalkit.py

--- myfunctionalkit.py
+++ myfunctionalkit.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 import os
 import time
 def clear():
     os.system("clear")
-cho=input("(1)PolyBar Refresh\n(2)Pacman And Pamac\n(3)Battery:")
+cho=input("(1)PolyBar Refresh\n(2)Pacman And Pamac\n(3)Battery\n(4)Ping\n(5)Weather\n:")
 if cho == "1":
     clear()
     os.system("sh /home/efe/.config/polybar/launch.sh")
@@ -17,7 +18,7 @@ elif cho == "2":
         if pac == "1":
             clear()
             pacin=input("What Do You Want To Install? :")
-            os.system("sudo pacman -Syyu "+pacin)
+            os.system("sudo pacman -Syyu "+pacin+)
         elif pac == "2":
             clear()
             pacse=input("What Do You Want To Search? :")
@@ -28,7 +29,7 @@ elif cho == "2":
             os.system("sudo pacman -Rs "+pacre)
     elif pa == "2":
         clear()
-        pam=input("(1)Install\n(2)Search\n(3)Remove")
+        pam=input("(1)Install\n(2)Search\n(3)Remove\b(4)Upgrade")
         if pam == "1":
             clear()
             pamin=input("What Do You Want To Install? :")
@@ -41,6 +42,15 @@ elif cho == "2":
             clear()
             pamre=input("What Do You Want To Remove? :")
             os.system("pamac remove "+pamre)
+        elif pamc == "4":
+            clear()
+            os.system("pamac upgrade")
 elif cho == "3":
     clear()
     os.system("upower -i /org/freedesktop/UPower/devices/battery_BAT0")
+elif cho == "4":
+    clear()
+    os.system("ping lukesmith.xyz")
+elif cho == "5":
+    clear()
+    os.system("curl wttr.in")
